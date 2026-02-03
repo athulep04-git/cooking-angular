7a6253e1d8ea4c6a5913759eecd2b9e12b36e868
athulep04-git <athulep04@gmail.com>
2026-02-03 17:44:56 +0530

    landingpage login register allrecipie view recipie contact about pages done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './pages/landing-page/landing-page';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Pnf } from './pages/pnf/pnf';
+import { AllRecipes } from './pages/all-recipes/all-recipes';
+import { ViewRecipe } from './pages/view-recipe/view-recipe';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:'',component:LandingPage
+    },
+    {
+        path:'login',component:Login
+    },
+    {
+        path:'register',component:Register
+    },
+    {
+        path:'all-recipes',component:AllRecipes
+    },
+    {
+        path:'view-recipe/:id',component:ViewRecipe
+    },
+    {
+        path:'about',component:About
+    },
+    {
+        path:'contact',component:Contact
+    },
+    {
+        path:'**',component:Pnf
+    }
+    
+];
---
+++ src/app/pages/all-recipes/all-recipes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-recipes',
+  imports: [],
+  templateUrl: './all-recipes.html',
+  styleUrl: './all-recipes.css',
+})
+export class AllRecipes {
+
+}
